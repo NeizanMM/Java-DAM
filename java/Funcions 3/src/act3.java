268eeb5a7ebf12e7e01617f3b39c9acfd626c0a8
NeizanMM <nmontesd@inscaparrella.cat>
2026-01-14 10:46:13 +0100

    3

--- java/Funcions 3/src/act3.java
+++ java/Funcions 3/src/act3.java
@@ -5,24 +5,29 @@ public class act3 {
     public static void main (String[] args) {
         act3 programa = new act3();
         programa.inici();
-
     }
+
     public static void inici () {
         String paraula = "";
         System.out.println("Introdueix un palindrom");
-        paraula = entrada();
-        System.out.println(comprovant(paraula));
-
+        do {
+            paraula = entrada();
+        } while (comprovant(paraula) != true);
+        System.out.println("La paraula entrada: " + paraula + " es " + comprovant(paraula));
     }
+
     public static String entrada () {
         Scanner sc = new Scanner(System.in);
         String paraula = sc.nextLine();
         return paraula;
     }
+
+
     public static boolean comprovant (String paraula) {
         char selector;
         String reves = "";
         boolean correcte = false;
+
         for (int i = 0; i < paraula.length() ; i++) {
             selector = paraula.charAt(i);
             reves = selector + reves;
@@ -31,9 +36,7 @@ public class act3 {
             correcte = true;
         } else {
             System.out.println("torna ha introduir una paraula");
-            entrada();
         }
         return correcte;
     }
-
 }
